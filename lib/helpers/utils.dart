cb7499645986199c205bb5705ec38636a73b4952
Aruhant <125254014+aruhant@users.noreply.github.com>
2021-02-05 14:54:57 +0530

    Fixed initial value for country dropdown

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -1,1 +1,30 @@
+import 'package:devicelocale/devicelocale.dart';
+import 'package:get/instance_manager.dart';
+import 'package:logger/logger.dart';
 
+extension GetLogger on Logger {
+  static Logger to = Get.find();
+}
+
+Future<String> getCountry() async {
+  Logger log = GetLogger.to;
+  Duration timeoffset = DateTime.now().timeZoneOffset;
+  log.v(
+      "Using timeoffset ${DateTime.now().timeZoneOffset} ${DateTime.now().timeZoneName}");
+  log.v(await Devicelocale.currentLocale);
+  if (timeoffset == Duration(hours: 5, minutes: 30)) {
+    log.v('Country from timeoffset India');
+    return 'IN';
+  }
+  if (timeoffset == Duration(hours: 9, minutes: 0)) {
+    log.v('Country from timeoffset Japan');
+    return 'JP';
+  }
+
+  String locale = await Devicelocale.currentLocale;
+  if ((locale != null) && (locale.length >= 5))
+    locale = locale.substring(locale.length - 2, locale.length);
+  log.v("Country from locale: ${locale}");
+  if (['IN', 'JP', 'US', 'CA', 'GB'].contains(locale)) return locale;
+  return 'IN';
+}
--- lib/ui/auth/sign_in_ui.dart
+++ lib/ui/auth/sign_in_ui.dart
@@ -8,70 +8,103 @@ import 'package:fire_starter/ui/auth/auth.dart';
 import 'package:fire_starter/ui/components/components.dart';
 import 'package:fire_starter/helpers/helpers.dart';
 import 'package:fire_starter/controllers/controllers.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:logger/logger.dart';
 
 class SignInUI extends StatelessWidget {
   final AuthController authController = AuthController.to;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
-    final labels = AppLocalizations.of(context);
-
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  LogoGraphicHeader(),
-                  SizedBox(height: 48.0),
-                  FormInputFieldWithIcon(
-                    controller: authController.phoneController,
-                    iconPrefix: Icons.phone,
-                    labelText: labels?.auth?.phone,
-                    validator: Validator(labels).number,
-                    keyboardType: TextInputType.phone,
-                    onChanged: (value) => null,
-                    onSaved: (value) =>
-                        authController.phoneController.text = value,
+      body: FutureBuilder(
+          future: getCountry(),
+          builder: (context, snapshot) {
+            return buildForm(context, snapshot.data);
+          }),
+    );
+  }
+
+  Form buildForm(BuildContext context, String country) {
+    final labels = AppLocalizations.of(context);
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                LogoGraphicHeader(),
+                SizedBox(height: 48.0),
+                InternationalPhoneNumberInput(
+                  onInputChanged: (PhoneNumber number) {
+                    // print(number.phoneNumber);
+                  },
+                  // onInputValidated: (bool value) {
+                  //   print(value);
+                  // },
+                  selectorConfig: SelectorConfig(
+                    selectorType: PhoneInputSelectorType.DIALOG,
                   ),
-                  FormVerticalSpace(),
-                  PrimaryButton(
-                      labelText: labels?.auth?.signInButton,
+                  ignoreBlank: true,
+                  autoValidateMode: AutovalidateMode.onUserInteraction,
+                  // selectorTextStyle: TextStyle(color: Colors.black),
+                  textFieldController: authController.phoneController,
+                  // formatInput: false,
+                  countries: ['IN', 'US', 'CA', 'JP'],
+                  // keyboardType: TextInputType.numberWithOptions(
+                  //     signed: true, decimal: false),
+                  inputBorder: OutlineInputBorder(),
+                  initialValue: PhoneNumber(isoCode: country),
+                  onSaved: (PhoneNumber number) {
+                    print('On Saved: $number');
+                  },
+                ),
+                FormInputFieldWithIcon(
+                  controller: authController.phoneController,
+                  iconPrefix: Icons.phone,
+                  labelText: labels?.auth?.phone,
+                  validator: Validator(labels).number,
+                  keyboardType: TextInputType.phone,
+                  onChanged: (value) => null,
+                  onSaved: (value) =>
+                      authController.phoneController.text = value,
+                ),
+                FormVerticalSpace(),
+                PrimaryButton(
+                    labelText: labels?.auth?.signInButton,
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        authController.signInWithEmailAndPassword(context);
+                      }
+                    }),
+                FormVerticalSpace(),
+                if (GetPlatform.isAndroid)
+                  LabelButton(
+                      labelText: labels?.auth?.googleSignIn,
                       onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          authController.signInWithEmailAndPassword(context);
+                        try {
+                          await authController.signInWithGoogle();
+                          Get.toNamed('home');
+                        } catch (e) {
+                          Get.snackbar('Error', e.toString());
                         }
                       }),
-                  FormVerticalSpace(),
-                  if (GetPlatform.isAndroid)
-                    LabelButton(
-                        labelText: labels?.auth?.googleSignIn,
-                        onPressed: () async {
-                          try {
-                            await authController.signInWithGoogle();
-                            Get.toNamed('home');
-                          } catch (e) {
-                            Get.snackbar('Error', e.toString());
-                          }
-                        }),
-                  if (GetPlatform.isMacOS || GetPlatform.isIOS)
-                    LabelButton(
-                        labelText: labels?.auth?.appleSignIn,
-                        onPressed: () async {
-                          try {
-                            await authController.signInWithApple();
-                            Get.toNamed('home');
-                          } catch (e) {
-                            Get.snackbar('Error', e.toString());
-                          }
-                        }),
-                ],
-              ),
+                if (GetPlatform.isMacOS || GetPlatform.isIOS)
+                  LabelButton(
+                      labelText: labels?.auth?.appleSignIn,
+                      onPressed: () async {
+                        try {
+                          await authController.signInWithApple();
+                          Get.toNamed('home');
+                        } catch (e) {
+                          Get.snackbar('Error', e.toString());
+                        }
+                      }),
+              ],
             ),
           ),
         ),
