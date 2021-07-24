6e02f73fee7687a771427b282f44c8d9f1734723
Aruhant <125254014+aruhant@users.noreply.github.com>
2021-07-24 13:43:31 +0530

    video view

---
+++ lib/ui/components/widgets/video_player_ui.dart
@@ -0,0 +1,56 @@
+import 'package:fire_starter/ui/components/widgets/topbar.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+
+class VideoApp extends StatefulWidget {
+  final String url;
+  const VideoApp({Key? key, required this.url}) : super(key: key);
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: TopBar(title: '', onBack: () => Navigator.pop(context)),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying ? _controller.pause() : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
